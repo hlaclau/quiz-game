99c848d95c3c7beb10d368c642a6553bb1bd5206
Hugo L <103247221+hlaclau@users.noreply.github.com>
2026-01-14 19:03:14 +0100

    feat: add open api documents with scalar (#37)

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,6 +1,7 @@
-import "dotenv/config";
 import cors from "@elysiajs/cors";
+import { swagger } from "@elysiajs/swagger";
 import { auth } from "@quiz-game/auth";
+import "dotenv/config";
 import { Elysia } from "elysia";
 import { routes } from "./presentation/router";
 
@@ -20,9 +21,23 @@ new Elysia()
 		}
 		return status(405);
 	})
+	.use(
+		swagger({
+			provider: "scalar",
+			path: "/docs",
+			documentation: {
+				info: {
+					title: "Quiz Game API Documentation",
+					version: "1.0.0",
+					description:
+						"An API documentation powered by Elysia and Scalar for the Quiz Game project",
+				},
+			},
+		}),
+	)
 	.use(routes)
 	.get("/", () => "OK")
 	.get("/api/health", () => "OK")
 	.listen(3000, () => {
-		console.log("Server is running on http://localhost:3000");
+		console.log("Server is running on port 3000");
 	});
