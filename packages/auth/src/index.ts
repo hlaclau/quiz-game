99315965f7377c7a1af83642a5f21fcf090517ae
hlaclau <hugo.laclau@gmail.com>
2026-01-16 00:42:53 +0100

    fix: clean authentication process and update infos on signin

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -2,7 +2,6 @@ import { db } from "@quiz-game/db";
 import * as schema from "@quiz-game/db/schema/auth";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { username } from "better-auth/plugins";
 
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
@@ -10,25 +9,13 @@ export const auth = betterAuth({
 		schema: schema,
 	}),
 	trustedOrigins: [process.env.CORS_ORIGIN || ""],
-	emailAndPassword: {
-		enabled: true,
-	},
 	socialProviders: {
 		discord: {
 			clientId: process.env.DISCORD_CLIENT_ID || "",
 			clientSecret: process.env.DISCORD_CLIENT_SECRET || "",
+			overrideUserInfoOnSignIn: true,
 		},
 	},
-	plugins: [
-		username({
-			minUsernameLength: 3,
-			maxUsernameLength: 20,
-			usernameValidator: (value) => {
-				// Only allow alphanumeric characters and underscores, no spaces or special chars
-				return /^[a-zA-Z0-9_]+$/.test(value);
-			},
-		}),
-	],
 	advanced: {
 		defaultCookieAttributes: {
 			sameSite: "none",
